c0df3ab618cf3dd9c7e5d48c3de323702e0c0f6b
MuntasirAsif <muntasirashifee@gmail.com>
2025-12-31 01:43:34 +0600

    feat: maps

---
+++ 09maps/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maos in golang");
+
+	languages := make(map[string]string);
+
+	languages["JS"] = "JavaScript"
+	languages["RB"] = "Ruby"
+	languages["PY"] = "Python"
+
+	fmt.Println("List of all languages: ", languages)
+	fmt.Println("Length of map: ", len(languages))
+
+	delete(languages, "RB")
+
+	fmt.Println("List of all languages after delete: ", languages)
+
+	fmt.Println("Js shorts for: ", languages["JS"])
+	
+	for key, value :=range languages{
+		fmt.Println("Key:", key, "Value:", value)
+	}
+}
